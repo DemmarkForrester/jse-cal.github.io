919873378cb665c4a38d44921e40975ac8db5fe9
AshineFoster <ashinefoster@gmail.com>
2021-03-17 16:18:49 -0500

    add treemap

---
+++ js/treemap.js
@@ -0,0 +1,569 @@
+market_cap = [{
+    "change": 0.06,
+    "close": 15.13,
+    "name": "SVL",
+    "value": "39901667030.38"
+}, {
+    "change": 0.05,
+    "close": 1.89,
+    "name": "AMG",
+    "value": "967480198.65"
+}, {
+    "change": 0.06,
+    "close": 3.06,
+    "name": "CPJ",
+    "value": "3366000000.00"
+}, {
+    "change": 0.0,
+    "close": 5.7,
+    "name": "GENAC",
+    "value": "5878125000.0"
+}, {
+    "change": 0.0,
+    "close": 1.39,
+    "name": "KLE",
+    "value": "139000000.00"
+}, {
+    "change": -0.04,
+    "close": 1.59,
+    "name": "PURITY",
+    "value": "354107581.89"
+}, {
+    "change": 0.0,
+    "close": 1.43,
+    "name": "PTL",
+    "value": "2205727924.40"
+}, {
+    "change": -0.16,
+    "close": 4.74,
+    "name": "KREMI",
+    "value": "1794412865.10"
+}, {
+    "change": -0.59,
+    "close": 18.05,
+    "name": "JSE",
+    "value": "12657562500.00"
+}, {
+    "change": 0.7,
+    "close": 29.72,
+    "name": "EPLY",
+    "value": "5720157876.00"
+}, {
+    "change": -0.13,
+    "close": 2.29,
+    "name": "CFF",
+    "value": "2059168755.70"
+}, {
+    "change": 0.46,
+    "close": 8.3,
+    "name": "XFUND",
+    "value": "18616942537.5"
+}, {
+    "change": 0.01,
+    "close": 2.55,
+    "name": "DTL",
+    "value": "11560069708.50"
+}, {
+    "change": 0.3,
+    "close": 48.87,
+    "name": "SJ",
+    "value": "190868378442.66"
+}, {
+    "change": 0.6,
+    "close": 4.74,
+    "name": "MDS",
+    "value": "1247368422.30"
+}, {
+    "change": 0.18,
+    "close": 6.4,
+    "name": "KEX",
+    "value": "3200000096.0"
+}, {
+    "change": -0.11,
+    "close": 2.8,
+    "name": "SIL",
+    "value": "1157639708.4"
+}, {
+    "change": -0.54,
+    "close": 5.4,
+    "name": "138SL",
+    "value": "2238300000.0"
+}, {
+    "change": 0.51,
+    "close": 32.28,
+    "name": "JMMBGL",
+    "value": "63125235732.96"
+}, {
+    "change": -0.25,
+    "close": 32.92,
+    "name": "ProvenJA",
+    "value": "20585138141.96"
+}, {
+    "change": 0.0,
+    "close": 9.64,
+    "name": "CAC",
+    "value": "1243870967.12"
+}, {
+    "change": 0.0,
+    "close": 4.7,
+    "name": "TTECH",
+    "value": "498200000.0"
+}, {
+    "change": 0.06,
+    "close": 3.36,
+    "name": "ROC",
+    "value": "719040000.00"
+}, {
+    "change": 0.02,
+    "close": 0.77,
+    "name": "JETCON",
+    "value": "449295000.00"
+}, {
+    "change": 0.0,
+    "close": 23.6,
+    "name": "ISP",
+    "value": "2478000000.0"
+}, {
+    "change": 0.44,
+    "close": 5.42,
+    "name": "KEY",
+    "value": "3031531207.42"
+}, {
+    "change": 0.0,
+    "close": 7.1,
+    "name": "PJX",
+    "value": "2200774653.1"
+}, {
+    "change": -0.21,
+    "close": 3.23,
+    "name": "MEEG",
+    "value": "969016150.00"
+}, {
+    "change": -2.22,
+    "close": 141.31,
+    "name": "NCBFG",
+    "value": "348578255224.68"
+}, {
+    "change": 0.0,
+    "close": 3.62,
+    "name": "ECL",
+    "value": "5927750000.00"
+}, {
+    "change": 0.0,
+    "close": 4.64,
+    "name": "SOS",
+    "value": "1160559120.00"
+}, {
+    "change": 0.15,
+    "close": 5.4,
+    "name": "FOSRICH",
+    "value": "2712287997.0"
+}, {
+    "change": -0.12,
+    "close": 0.83,
+    "name": "GWEST",
+    "value": "402424242.55"
+}, {
+    "change": -0.11,
+    "close": 15.57,
+    "name": "WISYNCO",
+    "value": "58387500000.00"
+}, {
+    "change": -0.03,
+    "close": 5.16,
+    "name": "VMIL",
+    "value": "7740129000.00"
+}, {
+    "change": 0.09,
+    "close": 2.99,
+    "name": "ELITE",
+    "value": "1056666000.00"
+}, {
+    "change": -0.25,
+    "close": 14.51,
+    "name": "SCIJMD",
+    "value": "5052958047.00"
+}, {
+    "change": -0.01,
+    "close": 0.9,
+    "name": "EFRESH",
+    "value": "702000000.0"
+}, {
+    "change": -0.02,
+    "close": 7.93,
+    "name": "MJE",
+    "value": "9525113877.63"
+}, {
+    "change": -0.04,
+    "close": 3.32,
+    "name": "INDIES",
+    "value": "4424021674.68"
+}, {
+    "change": 0.0,
+    "close": 5.34,
+    "name": "SML",
+    "value": "4046804136.60"
+}, {
+    "change": -0.01,
+    "close": 0.62,
+    "name": "SSLVC",
+    "value": "248000000.00"
+}, {
+    "change": 0.03,
+    "close": 5.9,
+    "name": "FTNA",
+    "value": "7371311467.5"
+}, {
+    "change": -0.83,
+    "close": 140.0,
+    "name": "MPCCEL",
+    "value": "3033315880.0"
+}, {
+    "change": -0.05,
+    "close": 0.79,
+    "name": "ICREATE",
+    "value": "156098075.00"
+}, {
+    "change": 0.0,
+    "close": 0.64,
+    "name": "WIG",
+    "value": "7040000000.00"
+}, {
+    "change": 0.11,
+    "close": 2.62,
+    "name": "LAB",
+    "value": "2477708460.24"
+}, {
+    "change": 0.0,
+    "close": 0.61,
+    "name": "SELECTF",
+    "value": "3111000000.00"
+}, {
+    "change": 0.0,
+    "close": 40.04,
+    "name": "CPFV",
+    "value": "5475171581.88"
+}, {
+    "change": -0.02,
+    "close": 0.93,
+    "name": "QWI",
+    "value": "1269450013.95"
+}, {
+    "change": 0.05,
+    "close": 3.35,
+    "name": "MAILPAC",
+    "value": "8375000000.00"
+}, {
+    "change": 0.06,
+    "close": 2.5,
+    "name": "LUMBER",
+    "value": "1765593750.0"
+}, {
+    "change": 0.03,
+    "close": 0.85,
+    "name": "SELECTMD",
+    "value": "3244120200.00"
+}, {
+    "change": 0.19,
+    "close": 16.21,
+    "name": "FIRSTROCKJMD",
+    "value": "620081130.00"
+}, {
+    "change": 0.1,
+    "close": 2.32,
+    "name": "CABROKERS",
+    "value": "609000000.00"
+}, {
+    "change": 0.0,
+    "close": 1.38,
+    "name": "TJH",
+    "value": "17251380000.00"
+}, {
+    "change": 0.0,
+    "close": 1.99,
+    "name": "CWJDEFERREDA",
+    "value": "895500000.00"
+}, {
+    "change": 0.01,
+    "close": 1.21,
+    "name": "TROPICAL",
+    "value": "1573000000.00"
+}, {
+    "change": 0.0,
+    "close": 19.88,
+    "name": "AFS",
+    "value": "5457255619.20"
+}, {
+    "change": -0.04,
+    "close": 80.22,
+    "name": "BIL",
+    "value": "87087080120.46"
+}, {
+    "change": 0.0,
+    "close": 13.2,
+    "name": "BRG",
+    "value": "2829055587.6"
+}, {
+    "change": -0.24,
+    "close": 3.82,
+    "name": "BPOW",
+    "value": "2158261800.00"
+}, {
+    "change": 0.0,
+    "close": 8.5,
+    "name": "CHL",
+    "value": "3538125000.0"
+}, {
+    "change": -1.78,
+    "close": 67.03,
+    "name": "CCC",
+    "value": "57051685694.73"
+}, {
+    "change": -0.13,
+    "close": 9.01,
+    "name": "CAR",
+    "value": "43738144000.00"
+}, {
+    "change": 0.13,
+    "close": 0.52,
+    "name": "CBNY",
+    "value": "283920000.00"
+}, {
+    "change": 1.14,
+    "close": 9.15,
+    "name": "DCOVE",
+    "value": "3590701340.40"
+}, {
+    "change": -0.09,
+    "close": 0.99,
+    "name": "1834",
+    "value": "1199131388.73"
+}, {
+    "change": -0.32,
+    "close": 89.08,
+    "name": "GK",
+    "value": "88635707353.48"
+}, {
+    "change": 0.02,
+    "close": 5.08,
+    "name": "HONBUN",
+    "value": "2394036106.00"
+}, {
+    "change": 0.01,
+    "close": 29.98,
+    "name": "JBG",
+    "value": "35954306472.00"
+}, {
+    "change": 0.0,
+    "close": 23.0,
+    "name": "JP",
+    "value": "25809312828.0"
+}, {
+    "change": 0.0,
+    "close": 0.55,
+    "name": "JPS7",
+    "value": "231000.00"
+}, {
+    "change": -0.04,
+    "close": 3.07,
+    "name": "JAMT",
+    "value": "6503028657.39"
+}, {
+    "change": 0.0,
+    "close": 7.25,
+    "name": "KPREIT",
+    "value": "4913052392.75"
+}, {
+    "change": 0.0,
+    "close": 44.99,
+    "name": "KW",
+    "value": "64344679014.22"
+}, {
+    "change": 0.06,
+    "close": 3.83,
+    "name": "LASD",
+    "value": "13442699808.36"
+}, {
+    "change": -0.03,
+    "close": 2.59,
+    "name": "LASF",
+    "value": "3290753140.22"
+}, {
+    "change": -0.09,
+    "close": 4.3,
+    "name": "LASM",
+    "value": "17762926549.3"
+}, {
+    "change": 0.01,
+    "close": 5.63,
+    "name": "MIL",
+    "value": "6762470508.33"
+}, {
+    "change": -59.65,
+    "close": 639.35,
+    "name": "PAL",
+    "value": "918763851.80"
+}, {
+    "change": -3.58,
+    "close": 64.37,
+    "name": "PJAM",
+    "value": "68628712119.30"
+}, {
+    "change": -0.09,
+    "close": 4.42,
+    "name": "PULS",
+    "value": "28831448617.92"
+}, {
+    "change": 0.01,
+    "close": 1.47,
+    "name": "RJR",
+    "value": "3561056851.38"
+}, {
+    "change": 1.97,
+    "close": 44.46,
+    "name": "SALF",
+    "value": "4618651073.40"
+}, {
+    "change": -0.33,
+    "close": 42.72,
+    "name": "SGJ",
+    "value": "132926397876.48"
+}, {
+    "change": 1.35,
+    "close": 73.22,
+    "name": "SEP",
+    "value": "53710300723.10"
+}]
+
+// echarts.use(
+//     [TitleComponent, TooltipComponent, ToolboxComponent, TreemapChart, CanvasRenderer]
+// );
+
+let chartDom = document.getElementById('chart');
+let myChart = echarts.init(chartDom);
+let option;
+
+let temp = market_cap.map(x => ({ name: x.name, value: [x.value, (1 - (x.close / (x.close - x.change))) * -100] }));
+
+let visualMin = -100;
+let visualMax = 100;
+let visualMinBound = -40;
+let visualMaxBound = 40;
+
+convertData(temp);
+
+function convertData(originList) {
+    let min = Infinity;
+    let max = -Infinity;
+
+    for (let i = 0; i < originList.length; i++) {
+        let node = originList[i];
+        if (node) {
+            let value = node.value;
+            value[1] != null && value[1] < min && (min = value[1]);
+            value[1] != null && value[1] > max && (max = value[1]);
+        }
+    }
+
+    for (let i = 0; i < originList.length; i++) {
+        let node = originList[i];
+        if (node) {
+            let value = node.value;
+
+            // Scale value for visual effect
+            if (value[1] != null && value[1] > 0) {
+                value[2] = echarts.number.linearMap(
+                    value[1], [0, max], [visualMaxBound, visualMax], true
+                );
+            }
+            else if (value[1] != null && value[1] < 0) {
+                value[2] = echarts.number.linearMap(
+                    value[1], [min, 0], [visualMin, visualMinBound], true
+                );
+            }
+            else {
+                value[2] = 0;
+            }
+
+            if (!isFinite(value[2])) {
+                value[2] = 0;
+            }
+
+            if (node.children) {
+                convertData(node.children);
+            }
+        }
+    }
+}
+
+
+option = {
+    backgroundColor: '#222',
+    width: '100%',
+    height: '100%',
+    title: {
+        left: 'center',
+        text: 'Market Capitalization - Ordinary Shares JMD',
+        backgroundColor: '#222',
+        subtext: 'Price Change > 0: green; Price Change < 0: red; Price Change = 0: grey'
+    },
+    textStyle: {
+        // fontFamily: 'Roboto'
+    },
+    toolbox: {
+        feature: {
+            restore: {},
+            saveAsImage: {},
+            //            dataView: {
+            //                readOnly: true
+            //           }
+        }
+    },
+    series: [{
+        name: 'ALL',
+        top: 60,
+        left: 0,
+        right: 0,
+        type: 'treemap',
+        data: temp,
+        roam: 'pan',
+        levels: [
+            {
+                color: ['#942e38', '#aaa', '#269f3c'],
+                visualMin: visualMin,
+                visualMax: visualMax,
+                visualDimension: 2,
+                colorMappingBy: 'value',
+                itemStyle: {
+                    gapWidth: 1,
+                    borderColor: '#fff'
+                }
+            }
+        ],
+    }],
+    tooltip: {
+        formatter: function (info) {
+            let value = info.value;
+
+            let amount = value[0];
+            amount = amount
+                ? `${Intl.NumberFormat('en-JM', {
+                    style: 'currency',
+                    currency: 'JMD',
+                    maximumFractionDigits: 2,
+                }).format(amount)}`
+                : '-';
+
+            let change = value[1];
+            change = change
+                ? `${change.toFixed(2)}%`
+                : '-';
+
+            return `<div class="uk-text-center uk-text-bold">${echarts.format.encodeHTML(info.name)}</div>
+                Market Cap: &nbsp;${amount}<br>
+                Change: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${change}`;
+        }
+    },
+};
+
+myChart.setOption(option);
